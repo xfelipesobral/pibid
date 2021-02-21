5ddb992ad92fb9928f198f0ea3f9e19e6fe4496d
Felipe Sobral <xfelipesobral@gmail.com>
2021-02-21 02:29:32 -0300

    init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    // Define urls
+    $url_paginas = 'source/pages/';
+    $url_componentes = 'source/components/';
+    $url_css = 'source/css/';
+    $url_json = 'source/json/';
+
+    require_once $url_paginas.'/inicio.php';
---
+++ source/pages/inicio.php
@@ -0,0 +1,23 @@
+<?php
+    require_once $url_componentes.'topo.php';
+?>
+
+<div id='inicio'>
+    <div id='cabecalho'>
+        <div class='texto'>
+            <p>
+                Aqui você fica conectado às ações do PIBID do curso de Licenciatura em Matemática da UTFPR, Câmpus
+                Toledo. Em 2021, a CAPES lançou um novo edital para o Programa PIBID e as atividades foram desenvolvidas
+                ao longo do segundo semestre de 2018 a dezembro de 2019 no Colégio Presidente Castelo Branco - PREMEN,
+                no Colégio Antonio José Reis e no Colégio Jardim Gisele, todos localizados na cidade de Toledo - PR.
+            </p>
+        </div>
+        <div class='logo'>
+            <h1>logo pibid</h1>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once $url_componentes.'rodape.php';
+?>
