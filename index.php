abec517fa89982e7adb5aeab0065776587b9325d
Felipe Sobral <xfelipesobral@gmail.com>
2021-03-21 15:41:47 -0300

    initi

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Define urls
-    $url_paginas = 'source/pages/';
-    $url_componentes = 'source/components/';
-    $url_css = 'source/css/';
-    $url_json = 'source/json/';
-
-    require_once $url_paginas.'/inicio.php';
